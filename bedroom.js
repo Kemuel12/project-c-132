da223a74286d6e3062e79a840018e6a7c33f1509
Kemuel12 <73594961+Kemuel12@users.noreply.github.com>
2021-10-22 16:33:17 +0530

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,57 @@
+function preload()
+{
+   img= loadImage("bedroom.jpg");
+}
+
+function setup()
+{
+   canvas=createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Bedroom";
+}
+
+status =""
+function preload()
+{
+    img=loadImage("dog_cat.jpg");
+}
+
+function setup()
+{
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects"
+}
+
+function modelLoaded()
+{
+    console.log("Model is loaded");
+    status=true;
+    objectDetector.detect(img,gotResults);
+}
+
+function gotResults(error,results)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+
+    else
+    {
+        console.log(results);
+    }
+
+}
+function draw()
+{
+    image(img,0,0,640,420);
+    fill("#b50000");
+    text("Bedroom",70,100);
+    noFill();
+    stroke("#ff0000");
+    rect(45,70,300,330);
+
+}
